0e13398d72a6d42efe1512b9260c9e4493c0d5e8
Alexander Shaidurov <shaa01@mail.ru>
2019-06-03 20:08:37 +0700

    init

---
+++ shoppingmngr/src/main/java/ru/shaa/sbt/shoppingmngr/ShoppingmngrApplication.java
@@ -0,0 +1,13 @@
+package ru.shaa.sbt.shoppingmngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmngrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmngrApplication.class, args);
+    }
+
+}
