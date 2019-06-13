bf88c010c4192fa93df1c144ba9f2e585b129cb2
Alexander Shaidurov <shaa01@mail.ru>
2019-06-13 20:15:36 +0700

    Использовал Mock вместо TaskBaseRunOnce т.к. базовый репозиторий не должен знать о конкретном наслденике

---
+++ shoppingmngr/src/test/java/ru/shaa/sbt/shoppingmngr/repositories/TaskBaseRepositoryTests.java
@@ -0,0 +1,71 @@
+package ru.shaa.sbt.shoppingmngr.repositories;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.shaa.sbt.shoppingmngr.entities.ScheduleType;
+import ru.shaa.sbt.shoppingmngr.entities.TaskBase;
+import ru.shaa.sbt.shoppingmngr.entities.TaskRunOnce;
+import ru.shaa.sbt.shoppingmngr.repositories.TaskBaseRepository;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class TaskBaseRepositoryTests {
+    @Autowired
+    @Qualifier("TaskBaseRepository")
+    TaskBaseRepository taskRepository;
+
+    @Test
+    void testGetById()
+    {
+        TaskBaseRepository.TaskDTO task = taskRepository.loadTaskDTO(7);
+        Assertions.assertNotNull(task);
+    }
+
+    @Test
+    void testSave()
+    {
+        TaskRunOnce task = new TaskRunOnce(null, LocalDateTime.parse("2019-07-08T00:00"), LocalDateTime.parse("2019-07-23T00:00"), new ScheduleType(1, "RUN_ONCE", ""), LocalDateTime.parse("2019-07-08T14:40"));
+        taskRepository.save(task);
+        Assertions.assertNotNull(task.getId(), "У сохраненного задания отсутствует идентификатор");
+    }
+
+    @Test
+    void testUpdate()
+    {
+        //TaskRunOnce task = new TaskRunOnce(null, LocalDateTime.parse("2019-07-08T00:00"), LocalDateTime.parse("2019-07-23T00:00"), new ScheduleType(1, "RUN_ONCE", ""), LocalDateTime.parse("2019-07-08T14:40"));
+        TaskBase task = Mockito.mock(TaskBase.class);
+        ArgumentCaptor<Integer> taskIDCaptor = ArgumentCaptor.forClass(Integer.class);
+        when(task.getId()).thenReturn(null);
+        when(task.getBegDate()).thenReturn(LocalDateTime.parse("2019-07-08T00:00"));
+        when(task.getEndDate()).thenReturn(LocalDateTime.parse("2019-07-23T00:00"));
+        when(task.getScheduleType()).thenReturn(new ScheduleType(1, "RUN_ONCE", ""));
+
+        taskRepository.save(task);
+
+        Mockito.verify(task).setId(taskIDCaptor.capture());
+        Integer taskId = taskIDCaptor.getValue();
+        Assertions.assertNotNull(taskId);
+
+        when(task.getId()).thenReturn(taskId);
+        when(task.getBegDate()).thenReturn(LocalDateTime.parse("2019-08-08T00:00"));
+        when(task.getEndDate()).thenReturn(LocalDateTime.parse("2019-08-23T00:00"));
+        when(task.getScheduleType()).thenReturn(new ScheduleType(1, "WEEKLY", ""));
+
+        taskRepository.save(task);
+
+        TaskBaseRepository.TaskDTO taskDTO = taskRepository.loadTaskDTO(task.getId());
+
+        Assertions.assertEquals(taskDTO.begDate, task.getBegDate(), "Не совпадает BegDate");
+        Assertions.assertEquals(taskDTO.endDate, task.getEndDate(), "Не совпадает EndDate");
+        Assertions.assertEquals(taskDTO.schType, task.getScheduleType().getCode(), "Не совпадает schType");
+    }
+}
--- shoppingmngr/src/test/java/ru/shaa/sbt/shoppingmngr/repositories/TaskRunOnceRepositoryTests.java
+++ shoppingmngr/src/test/java/ru/shaa/sbt/shoppingmngr/repositories/TaskRunOnceRepositoryTests.java
@@ -1,4 +1,4 @@
-package ru.shaa.sbt.shoppingmngr;
+package ru.shaa.sbt.shoppingmngr.repositories;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
