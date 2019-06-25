51084c3fa9060bb55811e2c6d6a932610660a3f1
Alexander Shaidurov <shaa01@mail.ru>
2019-06-25 22:02:48 +0700

    Добавил в список покупок список планируемых покупок

--- shoppingmngr/src/main/java/ru/shaa/sbt/shoppingmngr/entities/PurchaseList.java
+++ shoppingmngr/src/main/java/ru/shaa/sbt/shoppingmngr/entities/PurchaseList.java
@@ -1,14 +1,20 @@
 package ru.shaa.sbt.shoppingmngr.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PurchaseList {
     Integer id;
     String caption;
     Owner owner;
 
+    List<PlannedPurchase> plannedPurchases;
+
     public PurchaseList(Integer id, String caption, Owner owner) {
         this.id = id;
         this.caption = caption;
         this.owner = owner;
+        this.plannedPurchases = new ArrayList<>();
     }
 
     public Integer getId() {
@@ -36,5 +42,12 @@ public class PurchaseList {
         this.owner = owner;
     }
 
+    public List<PlannedPurchase> getPlannedPurchases() {
+        return plannedPurchases;
+    }
+
+    public void setPlannedPurchases(List<PlannedPurchase> plannedPurchases) {
+        this.plannedPurchases = plannedPurchases;
+    }
 
 }
