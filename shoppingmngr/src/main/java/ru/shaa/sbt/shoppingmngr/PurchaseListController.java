0f364253b4c485689cd69d46f2c260f9f7cb86c8
Alexander Shaidurov <shaa01@mail.ru>
2019-07-06 11:47:12 +0700

    Удаление покупки по ID списка и ID покупки

--- shoppingmngr/src/main/java/ru/shaa/sbt/shoppingmngr/PurchaseListController.java
+++ shoppingmngr/src/main/java/ru/shaa/sbt/shoppingmngr/PurchaseListController.java
@@ -1,9 +1,12 @@
 package ru.shaa.sbt.shoppingmngr;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.shaa.sbt.shoppingmngr.entities.PlannedPurchase;
 import ru.shaa.sbt.shoppingmngr.entities.PurchaseList;
+import ru.shaa.sbt.shoppingmngr.repositories.IPlannedPurchaseRepository;
 import ru.shaa.sbt.shoppingmngr.repositories.IPurchaseListRepository;
 
 import java.util.List;
@@ -12,11 +15,13 @@ import java.util.List;
 @RequestMapping("/purchaselist")
 public class PurchaseListController {
     IPurchaseListRepository purchaseListRepository;
+    IPlannedPurchaseRepository plannedPurchaseRepository;
 
     @Autowired
-    public PurchaseListController(IPurchaseListRepository purchaseListRepository)
+    public PurchaseListController(IPurchaseListRepository purchaseListRepository, IPlannedPurchaseRepository plannedPurchaseRepository)
     {
         this.purchaseListRepository = purchaseListRepository;
+        this.plannedPurchaseRepository = plannedPurchaseRepository;
     }
 
     @GetMapping("/{id:[0-9]+}")
@@ -38,6 +43,23 @@ public class PurchaseListController {
 
         return result;
     }
+
+    @DeleteMapping("/{id:[0-9]+}/planned/{idPlannedPurchase:[0-9]+}")
+    public ResponseEntity<PlannedPurchase> deletePlannedPurchase(@PathVariable("id") int idList, @PathVariable("idPlannedPurchase") Integer idPlannedPurchase) {
+        PlannedPurchase result = null;
+        for (PlannedPurchase p:
+            purchaseListRepository.getById(idList).getPlannedPurchases()) {
+            if (p.getId() == idPlannedPurchase)
+                result = p;
+        }
+
+        if (result == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+        plannedPurchaseRepository.delete(result);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
 
     @GetMapping("/{id:[0-9]+}/planned")
     public List<PlannedPurchase> getPlannedPurchaseList(@PathVariable("id") int idList) {
